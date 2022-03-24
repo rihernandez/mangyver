0ddc0ec46dec3f44a9c99320851cc6943a5c2aad
rhc921004@gmail.com <rhc921004@gmail.com>
2022-03-24 09:42:26 -0400

    adding new SP for global request

--- src/repositories/process.repository.ts
+++ src/repositories/process.repository.ts
@@ -32,11 +32,23 @@ export const getProcesMobile = async (operationId?: string) => {
   return result;
 };
 
-export const getProcesss = async (
-  operationId?: string
-): Promise<Array<Process>> => {
-  const repository = getRepository(Process);
-  return repository.find({ where: [{ operation: operationId }] });
+// export const getProcesss = async (
+//   operationId?: string
+// ): Promise<Array<Process>> => {
+//   const repository = getRepository(Process);
+//   return repository.find({ where: [{ operation: operationId }] });
+// };
+
+export const getProcesss = async (operationId?: string) => {
+  const connection = getConnection();
+  const result = await connection.query(
+    "SP_Processes @operationId='" + operationId + "'"
+  );
+
+  if (!result) {
+    return { msg: "No hay Informacion disponible para mostrar." };
+  }
+  return result;
 };
 
 export const createProcess = async (
